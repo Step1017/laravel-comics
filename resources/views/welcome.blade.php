128ef5387a184f3ba447aeca9ab562c6baf19bf6
Step1017 <s.aveta@yahoo.com>
2023-03-10 19:11:00 +0100

    Fatto main

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,5 +3,24 @@
 @section('pageTitle') Home | Laravel Comics @endsection
 
 @section('pageContent')
-    
+    <div class="jumbotron"></div>
+    <div class="content-container">
+        <div class="container">
+            <div class="title">
+                <h2>CURRENT SERIES</h2>
+            </div>
+            <div class="card-container">
+                @foreach ($comics as $card)
+                    <div class="card">
+                        <div class="img-container">
+                            <img src="{{ $card['thumb'] }}" alt="{{ $card['title'] }}">
+                        </div>
+                        <h4>{{ $card['title'] }}</h4>
+                    </div>
+                @endforeach
+
+            </div>
+            <button>LOAD MORE</button>
+        </div>
+    </div>
 @endsection
